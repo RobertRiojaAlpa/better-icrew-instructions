7451b3543941d8e65cd35288afe93edaf9c4492d
RobertRioja-Alpa <robert.rioja@alpa.org>
2026-08-28 20:05:55 -0400

    Update GMSettings.js

--- res/GMSettings.js
+++ res/GMSettings.js
@@ -1,4 +1,4 @@
-//v23
+//v24
 class GMSettings {
     static GMSetting = class {
         constructor(name, defaultValue) {
@@ -235,6 +235,11 @@ class GMSettings {
 	static async addPageResolverTestResult(page) {
 		let result = (await GMSettings.TEST_PAGE_RESOLVERS_RESULT.get() + "\n" + page + ": " + (await PageResolver.getMatchingPages(Menus.getMenu()))).trim();
         await GMSettings.TEST_PAGE_RESOLVERS_RESULT.set(result);
+	}
+	
+	static async addSettingsTestResult(setting, result) {
+		let result = ((await GMSettings.TEST_SETTINGS_RESULT.get()) + "\n" + setting + ": " + result).trim();
+        await GMSettings.TEST_SETTINGS_RESULT.set(result);
 	}
 
     static async getAllGMValues() {
